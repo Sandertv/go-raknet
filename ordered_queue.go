72afc39cf9e6aec0eafab9be3b922788aa4dd187
Sandertv <st.ten.veldhuis@gmail.com>
2020-06-05 16:16:10 +0200

    raknet/ordered_queue.go: Removed pointless checks for 0 sequence numbers.

--- ordered_queue.go
+++ ordered_queue.go
@@ -15,9 +15,6 @@ type orderedQueue struct {
 	timestamps   map[uint24]time.Time
 	lowestIndex  uint24
 	highestIndex uint24
-	lastClean    time.Time
-
-	zeroRecv bool
 
 	ptr    int
 	delays []time.Duration
@@ -30,12 +27,6 @@ func newOrderedQueue() *orderedQueue {
 
 // put puts a value at the index passed. If the index was already occupied once, an error is returned.
 func (queue *orderedQueue) put(index uint24, value interface{}) error {
-	if index == 0 && !queue.zeroRecv {
-		queue.zeroRecv = true
-		queue.queue[index] = value
-		queue.timestamps[index] = time.Now()
-		return nil
-	}
 	if index < queue.lowestIndex {
 		return fmt.Errorf("cannot set value at index %v: already taken out", index)
 	}
