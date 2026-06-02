060fb36a3a9a1fe9d31b73ed974df6d92ab1ab49
TrippleAWap <90356816+TrippleAWap@users.noreply.github.com>
2026-06-02 10:30:22 -0600

    Revert "Simplified logic for decrementing `blockCount`"
    This reverts commit 7aa336401b8dd95053f2454821b5397a8b1cce97.

--- listener.go
+++ listener.go
@@ -318,7 +318,7 @@ func (s *security) blocked(addr net.Addr) bool {
 	}
 	if !time.Now().Before(expiresAt) {
 		delete(s.blocks, ip)
-		s.blockCount.Add(-1)
+		s.blockCount.Store(uint32(len(s.blocks)))
 		return false
 	}
 	
